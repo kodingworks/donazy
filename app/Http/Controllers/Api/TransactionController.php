2b11d1620cc3c6ff5d2cd06360eb14b85a060b02
AmdaaRaijen <bintangtriadmaja@gmail.com>
2023-05-25 14:15:45 +0700

    add xendit/xendit-php

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Xendit\Xendit;
 
 class TransactionController extends Controller
 {
-    //
+    private $payement;
+    public function __construct()
+    {
+        Xendit::setApiKey('xnd_development_...');
+    }
 }
