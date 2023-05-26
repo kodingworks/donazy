17277681430c2fe1acad5c70076486a330c0cc23
AmdaaRaijen <bintangtriadmaja@gmail.com>
2023-05-26 08:01:46 +0700

    create transactionApi Model

---
+++ app/Models/TranasctionApi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TranasctionApi extends Model
+{
+    use HasFactory;
+}
